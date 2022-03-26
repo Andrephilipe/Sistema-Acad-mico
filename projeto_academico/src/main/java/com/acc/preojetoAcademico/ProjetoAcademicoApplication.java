b679621fedb9c77682191a308c5bb39cb8491d08
André Cassiano <andre.p.cassiano@accenture.com>
2022-03-26 08:55:46 -0300

    Primeiros escopos do projeto

---
+++ projeto_academico/src/main/java/com/acc/preojetoAcademico/ProjetoAcademicoApplication.java
@@ -0,0 +1,13 @@
+package com.acc.preojetoAcademico;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PreojetoAcademicoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PreojetoAcademicoApplication.class, args);
+	}
+
+}
---
+++ projeto_academico/src/test/java/com/acc/preojetoAcademico/PreojetoAcademicoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.acc.preojetoAcademico;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PreojetoAcademicoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
