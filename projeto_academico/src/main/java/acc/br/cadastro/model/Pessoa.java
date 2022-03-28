02e6b187b3e856bc29a85e9942335a7f213f99c9
André Cassiano <andre.p.cassiano@accenture.com>
2022-03-28 10:03:20 -0300

    Add escopo disciplina

--- projeto_academico/src/main/java/acc/br/cadastro/model/Pessoa.java
+++ projeto_academico/src/main/java/acc/br/cadastro/model/Pessoa.java
@@ -1,6 +1,8 @@
 package acc.br.cadastro.model;
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
@@ -9,16 +11,18 @@ import javax.persistence.Table;
 public class Pessoa {
 
     @Id
-    @Column
-    private int id;
+    @GeneratedValue(strategy=GenerationType.SEQUENCE, generator="ID_SEQ")
+    @Column(name="id")
+    private Integer id;
+
     private String nome;
     private int cpf;
     private String sexo;
     private String tipo;
-    public int getId() {
+    public Integer getId() {
         return id;
     }
-    public void setId(int id) {
+    public void setId(Integer id) {
         this.id = id;
     }
     public String getNome() {
